b733a6f9684d53070f56c052ca61cccae1ab8650
Naimur Rahman Dorjoy <rahmandurjoy9@gmail.com>
2025-06-12 00:21:57 +0600

    Updating Data fethcing

--- Javascript/dataFetching.js
+++ Javascript/dataFetching.js
@@ -10,9 +10,11 @@
 })();
 
 // Retrive single user
+//N.B. fetch doesn't give path, or not found type error. It just send 1. DNS server error, 2. No internet Error 3. CORS rejection etc.
 (()=>{
     fetch('https://jsonplaceholder.typicode.com/posts/1')
     .then(res => res.json())
     .then(user => console.log(user))
+    .catch(error => console.error(error.message))
 
 })()
